9ddb8b05059064c3469fce93e90c8031a2fa992a
vinh <tranvinh@hotmail.com.vn>
2018-06-01 14:19:08 +0700

    fist edition

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const purify = require('gulp-purifycss');
+
+
+gulp.task('hello',()=>{
+  console.log('hello vinh');
+})
+
+gulp.task('sass',()=>{
+  return gulp.src('app/scss/*.scss')
+             .pipe(sourcemaps.init())
+             .pipe(sass().on('error',sass.logError))
+             .pipe(sourcemaps.write())
+             .pipe(gulp.dest('app/css'))
+             .pipe(browserSync.stream());
+
+});
+
+
+/* gulp.task('watch',()=>{
+  gulp.watch('app/scss/*.scss',gulp.series('sass'));
+}); */
+
+gulp.task('browserSync',()=>{
+  browserSync.init({
+    server: {
+      baseDir: 'app',
+    }
+  })
+});
+
+gulp.task('serve', ()=>{
+  browserSync.init({
+    server: 'app'
+  });
+  gulp.watch('app/scss/*.scss',gulp.series('sass'));
+  gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', () =>
+    gulp.src('app/css/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'))
+);
+
+
+gulp.task('purify', function() {
+  return gulp.src('app/css/main.css')
+    .pipe(purify(['app/**/*.js', 'app/**/*.html']))
+    .pipe(gulp.dest('./dist/'));
+});
