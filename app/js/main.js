27c7a6da09caf01bcdfd56196f40bca3c82937c2
vinh <tranvinh@hotmail.com.vn>
2018-06-03 20:40:42 +0700

    check point 3

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
-  console.log('ready');
-
   $('.testimonial-slider').slick({
     dots: true,
     nextArrow: null,
     prevArrow: null,
     asNavFor: '.bg-slider',
+    accessibility: 'false',//fix scroll jump when click dots
+
   });
 
   $('.bg-slider').slick({
     autoplay:false,
     asNavFor: '.testimonial-slider',
+    accessibility: 'false',//fix scroll jump when click dots
+
   })
 })
